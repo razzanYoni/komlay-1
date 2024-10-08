b0caf7ae1abe777a0886d8cc1fdf39591a8be0bd
razzanYoni <13521087@std.stei.itb.ac.id>
2024-10-08 21:05:27 +0700

    feat: add some services

--- web-service/serve-the-food/src/app.ts
+++ web-service/serve-the-food/src/app.ts
@@ -5,10 +5,10 @@ import { Client, logger } from "camunda-external-task-client-js";
 const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger, asyncResponseTimeout: 10000 };
 const client = new Client(config);
 
-client.subscribe('calculate-price', async function({ task, taskService }) {
+client.subscribe('serve-the-food', async function({ task, taskService }) {
   // Put your business logic here
 
-  const orders = task.variables.getAll();
+  const orders = task.variables.get('orders');
 
   console.log(`${orders.length} orders to calculate the price for...`);
 
